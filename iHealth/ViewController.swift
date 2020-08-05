b7c890f965f708cfa9e5c90b36452a919e3022df
Stephanie Gao <stephaniesgao@gmail.com>
2020-08-05 16:55:32 -0600

    Initial Commit

---
+++ iHealth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iHealth
+//
+//  Created by Stephanie Gao on 8/5/20.
+//  Copyright © 2020 Stephanie Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
